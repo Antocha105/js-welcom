69bd5804801e38fd204d9905c16b202a270d597d
Chuprin Anton <workchuprin105@gmail.com>
2023-12-27 15:17:19 +0200

    let+const

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
-/*
-Змінна
-ключове слово імʼя змінної
+let box1="Hello"
 
-var - deprecated
-let - читати і записувати
-const - читати
+let box2; //undefined
 
-*/
+box2=15; //15
 
-let box;
+const secretWord='superSecret!Kuivstar!Cybersecuruti'
 
-// "=" оператор присвоєння
+console.log(secretWord);
 
-box=3; //операція присваювання
-
-console.log(box); //3
-
-//Молжемо визначати значення змінної при створенні
-
-let box2="Hello world";
-
-console.log(box2);
-
-box2=236
-
-console.log(box2);//236
-
-let isActive = true;
-let islogged = false
-
-//Присвоення значення із іншої змінної
-
-let x=10;
-let y=x;
-console.log(y)  //10
-/*
-    Правила неймінгу змінних
-
-    1.Вікористовуйте осмісленні та описові імена змінніх
-    Наприклад:
-     userName, age, totalCase
-     2.Використовуйте  camelCase у JS
-
-     3.Уникайте використання однобуквенних або непояснювальніх скорочень
-     Наприклад:
-
-     x, y, zxc
-
-     4. Уникайте пробілів та спеціальних символів у назвах змінних.
-     5. Заборонено починати назву змінної з цифри
-
-
-    */
+secretWord='worrrd'; //ПЕРЕВИЗНАЧАТИ КОНСТАНТУ НЕ МОЖНА 
