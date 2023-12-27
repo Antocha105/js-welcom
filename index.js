be02fd4c44b29331ce53b520793bd5a06be07b92
Chuprin Anton <workchuprin105@gmail.com>
2023-12-27 14:27:32 +0200

    Data types

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ alert("Hello world");
 
 //Інтерфейс - місце взаємодії між користувачем та системою
 
+//однорядковій коментар
 
-Hello world
+/*багаторядковий коментар*/
+
+
+console.log(true); //boolean
+console.log('this is text') // string
+console.log(50);// number
+console.log(null);
+console.log(undefined);
+console.log(5n)// bigint
