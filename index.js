96f4033b322cc09e1e244c0c021b45fd15a7b230
Chuprin Anton <workchuprin105@gmail.com>
2023-12-28 22:26:00 +0200

    i

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ console.log(rest);
 const x3=2**9;
 console.log(x3);
 
+// Increment decrement
+
+let superNumber=1
+console.log(superNumber++); //1+1=2
+console.log(superNumber--); //1-1=0
+console.log(++superNumber); //1+1=2
+console.log(--superNumber); //1-1=0
+
+let k=0;
+k=k+3
+k=k-2
+
+
+k+=3
+k-=2
+k*=4
+k/=5
+
+
+
 
 
 
