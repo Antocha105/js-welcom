e693b04c9fd1b8f00f7b41a3832c157f5f5078ff
Chuprin Anton <workchuprin105@gmail.com>
2023-12-29 17:30:28 +0200

    Add string variable with r value

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 let length=3
 let width=8
 
@@ -18,3 +18,29 @@ if(2+3===5){
 if(2+2===5){
     console.log('Числа рівні між собою!'); ///виконаеться якщо умова true
 }else{'Числа рівні між собою!'}; ///виконаеться якщо умова false
+
+
+let a=3
+let b=6
+let n=8
+
+if (b%2===0) { n+=5
+    
+}else{n=15}
+
+console.log(a,b,n)
+
+
+if-else if-else
+*/
+
+let r=6
+let strWithR='r='+r;
+
+if(r>12){
+    console.log(strWithR + ' більше 12');
+}else if(r<5){
+    console.log(strWithR + ' менше 5')
+}else{console.log(strWithR + ' в діапазоні між 5 і 12')};
+
+
