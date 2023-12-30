a110a7e3c8296071f3afb79f604c5c0615b1c7fc
Chuprin Anton <workchuprin105@gmail.com>
2023-12-30 17:33:45 +0200

    Practice task resolve

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ if(isWeekend===true||isHoliday===true){
 }else{
     console.log('Треба працювати');
 }
-*/
+
 
 const number=10;
 
@@ -74,4 +74,78 @@ if(number!==5){
     console.log(number + ' число не дорівнює5');
 }else{
     console.log(number + ' число дорівнює 5')
+}
+
+
+
+const min=45;
+if(min>=0 && min <15){
+    console.log('Перша четверть')
+}else if (min >=15 && min <=30){
+    console.log('Друга четверть')
+}else if(min>=30 && min<=45){
+    console.log('Третя четверть')
+}else if(min>=45 &&min<60){
+    console.log('Четверта четверть')
+}else{
+    console.log('виникла помілка');
+}
+*/
+
+/*
+const year=2012
+
+if(year%4===0 && year %100 !==0 || year % 400 === 0){
+    console.log(year+ 'Є високосним');
+}else{
+    consol.log(year+ ' Не є високосним');
+}
+*/
+
+
+/*
+const left = 10;
+const right = 20;
+
+const number=15;
+
+if(number >= left && number <= right){
+    console.log(number + 'є' + '['+left+';'+right+'];');
+}else{
+    consol.log('Поза діапазоном')
+}
+*/
+
+
+/*
+const n=20;
+const m=50;
+const k=50;
+
+if(m>=n && k>=n){
+    console.log('так зможе нагородити');
+}else{console.log('Ні, не зможе нагородити')
+
+}
+*/
+
+const age=5;
+const hasDiscount=true;
+
+let ticketPrice
+
+if(age<0 || typeof age !=='number'){
+    console.log('Ви ввели некоректний вік користувача. Спробуйте ще раз.')
+}else if(age<=6){
+    ticketPrice=0;
+}else if(age<=12){
+    ticketPrice=10;
+}else if(!hasDiscount){
+    ticketPrice=20
+}else{
+    ticketPrice=20/2;
+}
+
+if(ticketPrice !==undefined){
+    console.log('Вартість квитка складає:' + ticketPrice +'$')
 }
