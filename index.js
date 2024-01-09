8c98bc6f65a7e0d59ecefd20bcc5731285cdf3e9
Chuprin Anton <workchuprin105@gmail.com>
2024-01-09 21:35:47 +0200

    do while

--- index.js
+++ index.js
@@ -1,90 +1,22 @@
+// loop - цикли. 
+
 /*
-Задача.
+while - цикл з передумово
+do-while - цикл з післяумовою.
 
-ПОтрібно написаті в когсолі 10 разів цифру 1.
+do{
+    тут буде блок коду який циклічно виконується
+} while(умова)
 
-while(кількість_консоль_логів < 10){
-    console.log(1);
-    КІлькість консоль логів++
-
-}
+                       
 
 
 */
 
- let iterator = 0; // Лічильник консоль логів
+const correktPassword = 'qwerty1234';
 
- //Ітерація - один прохід циклу
-/*
-
- while(iterator<10){
-    console.log(iterator);
-    iterator++
- }
- */
- //Нескінченний цикл
-
- /*
- while(true){
-    console.log('Не вірно');   //Один раз не вірно буде віведено в консоль
-    break;
-
- }
- //Задача  зробити 10 повторів (ітерацій) циклу і вивести на консоль парні значення лічильника
-
- let i = 0;
-
- while(i < 10){
-    if(i%2===0){
-        console.log(i);
-    }
-    i++;
- }
-*/
-/*
-
-let i = 0;
-
-while(true){ //Безкінечний цикл
-    i++;
-}
-*/
-
-/*
-Дано правільній пароль = qwerty1234
-
-Запитуйте у користувача через промт пароль поки він не співпаде
-
-*/
-
-const correctPass = 'qwerty1234'
-
-
-let userPass  = prompt('Введіть свій пароль');
-
-while(userPass !==correctPass){
-    userPass = prompt('помилка! Ввведіть ще раз.')
-}
-
-
-/*
-while (true){
-    const userPass === prompt('Введіть свій пар');
-    if(userPass === correctPassword){
-        break;
-    }
-}
-*/
-/*
-let isPasswordCorrect = false;
-
-
-while (!isPasswordCorrect) {
-    const userPass = prompt('Введіть свій пароль');
-
-    if(userPass === correctPass){
-        isPasswordCorrect = true;
-    }
-    
-}
-*/
+let userPass;
+do{
+    userPass = prompt('Введіть свій пароль');
+}while(userPass !==correctPassword)
+//Дотих пір доки пароль з промпту не дорівнює корект пассворд буде виконуватися юсер пасс
