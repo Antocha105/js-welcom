0b41fe384de5f37678279b76f66ad412e587ba65
Chuprin Anton <workchuprin105@gmail.com>
2024-01-15 22:59:04 +0200

    Objeckt welcome

--- index.js
+++ index.js
@@ -1,76 +1,41 @@
-// loop - цикли. 
-
-
 /*
-Задача
-Написати функцію яка обчислює факторіал переданого числа, результат виводить на консоль
+Обєкти
 
-Якщо відемне число - поверніть помилку
-Якщо число = 0 або = 1 - поверніть з функції 1
-
-*/
-/*
-function factorial(number){
-    let res = 1; // Уцій змінній ми будемо накопичувати результат
-    for(let it = 1; it <=number; it++) {
-        if (number===0 || number===1){
-            return 1;
-        }else if (number<0){
-            return null;
-        }else{
-            res = res*it;
-        }
-        
-       
-} 
-return res;
+const назва обєкту{
+    ключ: значення
 }
-console.log(factorial(5));
-*/
-/*
-Задача1
-Написати функцію яка приймає два числа(діапазон) і виводить на консоль всі числа з цього діапазону які діляться на 5
 
-
+Обєкти мають властивості і методі
 
 */
-/*
-function printNumberDivisibleBy5(start,end){
-    for(let i=start;i<=end;i++){
-        if (i%5===0){
-            console.log(i);
+
+const obj = {
+    color: 'white',
+    fontSize: 2
+}
+
+//ООП - Парадигма програмування, яка дозволяє створити програми з  використанням обʼєктів, які маютьвластивості і методи.
+
+const bulb = {
+    lightness: 200,
+    power: '200W',
+    cap: 'small',
+    on: function(){
+        return 'The light ON!'
+    },
+    off: function(){
+        return 'The light OFF!'
     }
-}
-}
 
-printNumberDivisibleBy5(10,50);
-*/
-/*
-Написати гру физз базз для 100 чисел
-Починаючи з 1, потім число поступово інкрементується.
-Якшо число ділиться на 3 - гравець каже Фізз
-Якщо число ділиться на 5 - нравець каже Базз
-Якщо число ділиться на 3 і на 5 - гравець просто називає це число
-
-
-*/
-
-function fizzBuzzGame(limit){
-    for(let i =1; i <=limit; i++){
-        if(i%3===0 && i%5===0){
-            console.log('FizzBuzz');
-        }
-        else if(i%3===0){
-            console.log('Fizz')
-        }
-        else if(i%5===0){
-            console.log('Buzz');
-        
-    }
-    else{
-        console.log(i);
-    }
-}
 }
 
-fizzBuzzGame(100)
+console.log(bulb.lightness)
+
+
+
+const dog ={
+    name: 'Tuzik',
+    'favorit Food': 'meat'
+}
+console.log(dog.name)
+console.log(dog["favorit Food"])
