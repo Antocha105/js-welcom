f174af1e6ca4647756d0b7d9322a3c9578eecbce
Chuprin Anton <workchuprin105@gmail.com>
2024-01-19 22:19:54 +0200

    Computed property

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
-/*
-Всередині алерту віконати математичній розрахунок: 2+2=4
+// [] - Обчислювальні властивості
 
-*/
-
-//alert('2+2='+(2+2))
-
-//Подвійні лапки: "Привіт"
-//Одинарні лапки: 'Привіт'
-//Зворотні лапки: ʼПРивітʼ -->Шаблонний вираз- дозволяють вбудовувати обчислювальні вирази в строчку.
-
-//Вираз вкладаеться в ${...вираз...}
-
-//Рядки які знаходяться у зворотніх лапках називаються шаблонніми рядками
-//const nickname = 'Vasia';
-
-//задача зробити алерт і привітатися з васею
-
-//alert('Привіт, '+ name + '!');
-
-//Вираз  вкладається в таку фігуру
-
-//alert(`Привіт, ${nickname}!`); //Шаблонній рядок
-
-//alert(`REZYLITAT: ${1+2}!`); 
-
-//Запитати у користувача 2 числа та вивести відповіть алертом
-
-//const first = Number(prompt('Enter first number:'))
-//const second = Number(prompt('Enter second number:'))
-
-//alert(String(first) + '+' + String(second) + '=' + String(first+second))
-
-//alert(`${first} + ${second} = ${first + second}`)
-
-
-
-function greetingUser(user){
-    return `Привіт ${user.name + user.surname},  у наступному році тобі виповниться ${user.age + 1}!`
-
-    
-
-
-}
 const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 19
+    name: 'John' ,// Ключ  'name'  значення 'john'
+    'favorite color':'red',
+    0: 'value'
 }
 
-greetingUser(user)
+// Ключем в обʼєкті може бути рядок або тип данних Sumbol
+
+console.log(user.name)
+
+// . - Оператор доступу за імʼям властивості(коли мова йде про валідні ідентифікатори)
+
+//user.'favorite color'; --> SyntaxError
+//user.0; --> SyntaxError
+
+//  [] - оператор доступу до обчислювальної властивості(коли мова йде про невалідні ідентифікатори)
+
+console.log(user['favorite color'])
+
+console.log(user[0])
+
+const someObj = {
+    2:10
+}
+
+console.log(someObj[2])
+
+console.log(someObj['2'])
