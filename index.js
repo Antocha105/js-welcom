5cd289ecc005f3b60b510c8b6ad5ce46256dd73e
Chuprin Anton <workchuprin105@gmail.com>
2024-01-31 00:52:22 +0200

    Add pop+tasck

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ function MyArray(){
     this.lenght =0;
 
     this.push = function(value){
-        //Значення додати у кінець масиву
-        this[this.lenght] = value;
-        this.lenght++;
-        return this.lenght
-    }
-}
+        for(let i=0; i < arguments.lenght; i++){
+
+            this[this.lenght] = value;
+            this.lenght++;
+        }
+            return this.lenght;
+        this.pop = function(){
+            if(this.lenght>0){
+                //1.Зберегти останій єлемент
+                const lastItem = this[this.lenght-1]
+                //2. Видалити останній елемент масиву
+                delete this[this.lenght-1]
+                //3.Зменшити довжину масиву на 1
+                this.lenght--;
+                return lastItem;
+            }else{
+                return undefined;
+            }}}}
+              
+
 
 const arr = newMyArray();
 arr.push[1]
