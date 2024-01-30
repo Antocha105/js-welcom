23f05410a749b040b33dd9ae7b2981a7d7548b32
Chuprin Anton <workchuprin105@gmail.com>
2024-01-30 22:38:44 +0200

    array filter

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-// array.sort - Метод сортування в масиві
-// array.sort([compareFunction]) - переставляє елементи в новому масиві за правилом
 
-const array = [5,3,9,21,5,1,24]
-array.sort()   //Сортування за зростанням(Unicode)
+    //array.filter - використовується для створення нового масиву які задовільняють певну умову
 
-function compareFunction(a,b){
-    if(b>a){
-        //4(a),6(b)
-        //Відсортівати за зростанням
-        return -1;
-    }else{
-        //b(10)<a(5);
-        //Відсортівати за зростанням
-        return 1;
+    //array.filter(callback)
+    //Колбек буде вікликатися для кожного елементу масиву
+    //ця функція буде повертитати true, false 
 
+
+    const numbersArray = [5,3,-9,21,-5,1,-2,4]
+
+    function filterFunction(item){
+        if(item>0){
+            return true;
+        }else(item<0){
+            return false;
+        }
     }
-}
+    const newNumbersArray = numbersArray.filter(filterFunction);
 
-// a- поточне переглянуте число
-    // в- наступне число яку слідує за поточним
-    // Функція компаратор повінна повернуте одне з трьох значень
-    // -1 - спочатку а потім б
-    // 0- елементи залишаються на місці
-    // 1 - спочатку б потім а
+    //Можемо скоротити код
 
-    array.sort(compareFunction)
-
-    const newNumbersArray = [3,4,1,0,-5,12,13]
-
-    function compareFunction(a,b){
-        return a-b;  //Сортування у порядку зростання
-    }
-
-    function compareFunction(a,b){
-        return b-a;  //Сортування у порядку спадання
+    function filterFunction(item){
+        return item > 0;
     }
