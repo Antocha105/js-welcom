a394b5db0d42a6c870103d802f7f4f60c1e4c1c2
Chuprin Anton <workchuprin105@gmail.com>
2024-02-19 21:06:03 +0200

    class practise

--- new_syntax.js
+++ new_syntax.js
@@ -20,3 +20,21 @@ const user2 = new User('Dari','Dane',23)
 3. Конструктор має бути тільки один
 4. Як і у функціях-конструкторах, назва класу пишеться з великої літери
 */
+const MIN_ZP = 7100;
+const WORK_DAYS =21;
+const MIN_RATE = MIN_ZP/WORK_DAYS;
+
+class Worker {
+    constructor(name,lastName,rate=MIN_RATE,days=WORK_DAYS){
+        this.name = name;
+        this.lastName = lastName;
+        this.rate = rate;
+        this.days = days;
+    }
+
+    getCompensetion(){
+        return this.rate*this.days
+    }
+}
+
+const worker1 = new Worker('Alex','Dane',100,22)
