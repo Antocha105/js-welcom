41a369335d2ddd5b95e0e98835cc8a73ec6e4ad7
Chuprin Anton <workchuprin105@gmail.com>
2024-02-25 19:05:47 +0200

    Exampl (student+) home tasck

--- index.js
+++ index.js
@@ -1,63 +1,130 @@
-class User {
-  constructor(name,surname,age){
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-  }
-
-  getFullName(){
-    return `${this.name} ${this.name}`;
-  }
-}\ t
-
-
-class Moderator extends User{
-  constructor(name,surname,age){
-    super(name,surname,age)
-  }
-
-  getFullName(){
-    return `${this.name} ${this.name}  ${this.age}`
-  } 
-
-  createPost(text){
-    console.log('Post successfully created!');
-  }
-  getFullName(){
-    return `${this.name} ${this.name}-->>${this.age}`
-  }
-
-  daletePost(id){
-    console.log('Post successfully deleted!')
-  }
-}
-
-class Admin extends Moderator {
-  constructor(name,surname,age){
-    super(name,surname,age)
-    this.uniquePrefix = uniquePrefix;
-    
-  }
-
-  makeModerator(userId){
-    console.log('Moderator sucessfully sett!')
-  }
-
-  deleteModerator(userId){
-    console.log('Moderator sucessfully deteted!')
-  }
-}
-
-class Support extends Admin{
-  constructor(name, uniquePrefix ){
-    super( name, null, null,)
+class Person{
+  constructor(fullName,birthYear,gender){
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+    this.gender = gender;
 
   }
-}
+//Setters
+  set fullName(value){
+    if(typeof value !=='string'){
+      throw new TypeError('Full name must be a string');
+    }
 
-const user = new  User('John', 'Doe', '32')
+    this._fullName = value
+  }
+    set birthYear(value){
+      this._birthYear = value
 
-const moderator = new Moderator('alex','Krane',45)
+    }
+  set gender(value){
+    if(typeof value !==' string'){
+      throw new TypeError('Gender must be a string')
+    }
 
-const admin = new Admin('Jane','Doe','50','Head of Sales')
+    this._gender = value;
+  }
 
+
+
+//Getters
+
+    get fullName(){
+      return this._fullName
+    }
+
+    get birthYear(){
+      return this._birthYear
+    }
+
+    get gender(){
+      return this._gender
+    }
+
+    //Methods
+
+    greeting(){
+      let prefix;
+
+      if(this.gender==='male'){
+        prefix = 'Mr'
+      }else if(this.gender==='female'){
+        prefix='mrs.'
+      }else{
+        prefix = prompt('How should we address you?');
+      }
+      return `Hello ${prefix} ${this.fullName}`
+
+    }
+
+
+
+  }
+
+  class Student extends Person{
+    constructor(fullName,birthYear,gender,admisionYear,studentId,averageGrade){
+      super(fullName,birthYear,gender)
+
+      this.admisionYear = admisionYear;
+      this.studentId = studentId;
+      this.averageGrade = averageGrade
+
+    }
+
+    //Setters
+    set admisionYear(value){
+
+      this._admisionYear = value;
+    }
+
+    set studentId(value){
+      this._studentId = value
+    }
+
+    set averageGrade(value){
+      if(value>100||value<0){
+        throw new RangeError('Average grade must[0;100]')
+      }
+      if(typeof value !=='number'){
+        throw new TypeError('Average must be number')
+      }
+
+      this._averageGrade=value;
+    }
+
+
+    //Getters
+
+    get admisionYear(){
+      return this._admisionYear
+    }
+
+    get studentId(){
+      return this._studentId;
+    }
+
+    get averageGrade(){
+      return this._averageGrade
+    }
+
+    //Methods
+
+    isExellentStudent(){
+      //VAriant1
+      if(this.averageGrade >=90){
+        return true;
+      }else{
+        return false;
+      }
+
+      //Variant2
+      //const result = this.averageGrade>=90 ? true : false;
+
+      //variant3
+      //return this.averageGrade>=90;
+    }
+      
+
+  }
+
+  const person = new Person('Ivanov Ivan Ivanovich',1985,'male')
