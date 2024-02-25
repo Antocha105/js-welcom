ac83e7da57ef6560e22fdb53c07a0ab0a244194c
Chuprin Anton <workchuprin105@gmail.com>
2024-02-25 15:00:59 +0200

    Extends Exampl v2

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
-class Animal{
-  constructor(nickname,color){
-    this.nickname = nickname;
-    this.color = color;
+class User {
+  constructor(name,surname,age){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
   }
 
-  eat(){
-    return `${this.nickname} is eating`;
+  getFullName(){
+    return `${this.name} ${this.name}`;
+  }
+}\ t
+
+
+class Moderator extends User{
+  constructor(name,surname,age){
+    super(name,surname,age)
   }
 
+  getFullName(){
+    return `${this.name} ${this.name}  ${this.age}`
+  } 
 
+  createPost(text){
+    console.log('Post successfully created!');
+  }
+  getFullName(){
+    return `${this.name} ${this.name}-->>${this.age}`
+  }
+
+  daletePost(id){
+    console.log('Post successfully deleted!')
+  }
 }
 
-
-class Dog extends Animal{
-  constructor(nickname,color,){
-    super(nickname,color)
-
+class Admin extends Moderator {
+  constructor(name,surname,age){
+    super(name,surname,age)
+    this.uniquePrefix = uniquePrefix;
+    
   }
-  
-  gav(){
-    return `${this.nickname} gav-gav-gav`;
+
+  makeModerator(userId){
+    console.log('Moderator sucessfully sett!')
+  }
+
+  deleteModerator(userId){
+    console.log('Moderator sucessfully deteted!')
   }
 }
 
-const dog = new Dog('Tuzik','black')
+class Support extends Admin{
+  constructor(name, uniquePrefix ){
+    super( name, null, null,)
+
+  }
+}
+
+const user = new  User('John', 'Doe', '32')
+
+const moderator = new Moderator('alex','Krane',45)
+
+const admin = new Admin('Jane','Doe','50','Head of Sales')
+
