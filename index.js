fb9c4ffbe6e54f59677537268e4d64b4e93d89a9
Chuprin Anton <workchuprin105@gmail.com>
2024-03-11 20:59:47 +0200

    set Practise

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const set = new Set();
+/*const set = new Set();
 
 set.add(1);
 set.add(4);
@@ -14,3 +14,14 @@ set.delete('Hello')
 const valuesIterator = set.values()
 
 const arrayFromSet  = [...set.values()]
+
+*/
+
+const arr1 = [2,3,3,5,6,77,77,5,1,1,12,12,13]
+const set = new Set(arr1)
+
+
+const arr2 = [...arr1.values()]
+
+//Variant2
+const arr3 = [...new Set(2,3,3,5,6,77,77,5,1,1,12,12,13).values()] 
