c6d0188eff015596a1aecd1f163ba4b7574d6d09
Chuprin Anton <workchuprin105@gmail.com>
2024-03-11 22:19:31 +0200

    destruction object

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-/*const set = new Set();
-
-set.add(1);
-set.add(4);
-set.add('Hello');
-
-
-console.log(set.has(1)) //true
-console.log(set.has(12)) //false
-
-set.delete('Hello')
-
-
-const valuesIterator = set.values()
-
-const arrayFromSet  = [...set.values()]
-
-*/
-
-const arr11 = [2,3,3,5,6,77,77,5,1,1,12,12,13]
-const set = new Set(arr11)
-
-
-const arr22 = [...arr11.values()]
-
-//Variant2
-const arr33 = [...new Set(2,3,3,5,6,77,77,5,1,1,12,12,13).values()] 
-
-
-const arr1 = [4,3,7,5,-11]
-const arr2 = [3,4,8,7,2,-11]
-
-
-function twoArray (arr1,arr2){
-    return [...new Set([...arr1, ...arr2])]
+const monitor = {
+    sizes:{
+        height:{
+            value:30,
+            scale: 'cm02'
+        },
+        width:{
+            value: 50,
+            scale: 'cm003'
+        }        
+    },
+    brightnass: 750,
+    refresh:{
+        value:144,
+        scale:'Ggrc'
+    },
+    color: 'black',
+    resolution:'4K'
 }
 
-const arrayWithoutDoubles = twoArray(arr1,arr2)
+
+//const height = monitor.sizes.height.value
+
+//const {resolution} = monitor
+
+console.log(resolution)
+
+const {color: monitorColor} = monitor;
+
+const {sizes:{height: {value: heightValue}, width:{value: widthValue}}} = monitor
+
+console.log(widthValue)
+console.log(heightValue)
+
+const {sizes:{height: {scale: heightScale}, width:{scale: widthScale}}} = monitor
+console.log(heightScale)
+console.log(widthScale)
+
+const{color, brightnass,resolution, ...restOfMonitor} = monitor
