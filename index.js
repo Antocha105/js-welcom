c4174fa0d92a5842f554593304fd592492690697
Chuprin Anton <workchuprin105@gmail.com>
2024-03-11 21:15:35 +0200

    set practise 2

--- index.js
+++ index.js
@@ -17,11 +17,22 @@ const arrayFromSet  = [...set.values()]
 
 */
 
-const arr1 = [2,3,3,5,6,77,77,5,1,1,12,12,13]
-const set = new Set(arr1)
+const arr11 = [2,3,3,5,6,77,77,5,1,1,12,12,13]
+const set = new Set(arr11)
 
 
-const arr2 = [...arr1.values()]
+const arr22 = [...arr11.values()]
 
 //Variant2
-const arr3 = [...new Set(2,3,3,5,6,77,77,5,1,1,12,12,13).values()] 
+const arr33 = [...new Set(2,3,3,5,6,77,77,5,1,1,12,12,13).values()] 
+
+
+const arr1 = [4,3,7,5,-11]
+const arr2 = [3,4,8,7,2,-11]
+
+
+function twoArray (arr1,arr2){
+    return [...new Set([...arr1, ...arr2])]
+}
+
+const arrayWithoutDoubles = twoArray(arr1,arr2)
