5f94d5f355ff3a933a5e1eb5e569a9982b33fb5e
Chuprin Anton <workchuprin105@gmail.com>
2024-03-12 20:05:28 +0200

    closure

--- index.js
+++ index.js
@@ -1,82 +1,31 @@
-const monitor = {
-    sizes:{
-        height:{
-            value:30,
-            scale: 'cm02'
-        },
-        width:{
-            value: 50,
-            scale: 'cm003'
-        }        
-    },
-    brightnass: 750,
-    refresh:{
-        value:144,
-        scale:'Ggrc'
-    },
-    color: 'black',
-    resolution:'4K'
+let value =10;
+
+
+
+
+function wrapper(){
+    let value = 20;
+    console.log('wrapper function', value)
+
+    return function log(){
+        console.log('log function', value)
+    }
+    
 }
 
-/*
-//const height = monitor.sizes.height.value
+wrapper();
 
-//const {resolution} = monitor
+function makeCounter2 (){
+    let number = 10;
 
-//console.log(resolution)
-
-const {color: monitorColor} = monitor;
-
-const {sizes:{height: {value: heightValue}, width:{value: widthValue}}} = monitor
-
-console.log(widthValue)
-console.log(heightValue)
-
-const {sizes:{height: {scale: heightScale}, width:{scale: widthScale}}} = monitor
-console.log(heightScale)
-console.log(widthScale)
-
-const{color, brightnass,resolution, ...restOfMonitor} = monitor
-*/
-const user = {
-    name:'John',
-    age: 33,
-    adress:{
-        city: 'Kyev',
-        country:'Ukraine'
-    },
-    contacts:{
-        email:'john@email.com',
-        phone:'+380990973451'
+    return {
+        increment(){
+            return number+17,5;
+        },
+        decrement(){
+            return number-17,5;
+        }
     }
 }
 
-const {name, adress:{city}, contacts:{phone}} = user
-
-console.log(name)
-console.log(city)
-console.log(phone)
-
-
-
-const arr = [1,2,3,4,5,6]
-
-const[firstElement,secondElement]= arr;
-
-
-//old sintax
-
-function getFullName ({firstName, lastName,...restObj}){
-    return `${firstName} ${lastName}`
-}
-
-
-const user2 ={
-    firstName:'John',
-    lastName:'Doe',
-    age:42,
-    geolocation:'56365432465',
-    brouwser:'crome'
-}
-
-getFullName(user2)
+const fnObj = makeCounter2;
